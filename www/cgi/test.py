c7abc883dc612b78b4b17523247027fb798b2955
lecricreator <loucamorales@orange.fr>
2026-07-06 17:05:15 +0200

    build: add cgi py and sh

---
+++ www/cgi/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+print("Content-Type: text/html")
+print()
+print("<html><body style='text-align:center;'>")
+print("PYTHON PROGRAMMING")
+print("<h1 style='color: pink;'>This is the most shitty CGI !!!</h1>")
+print("</body></html>")
