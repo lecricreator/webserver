924a2f8c32fdaa622541d22e7e7afc2d6f8d6b2e
Saël Jad Mokhtari <samokhta@r2s3.lab.local>
2026-07-28 17:00:11 +0200

    feature: deleteRequest() done already??? this feels wrong

--- src/requestHandler/delete.cpp
+++ src/requestHandler/delete.cpp
@@ -2,5 +2,20 @@
 
 bool    httpRequest::deleteRequest()
 {
-    
+    if (remove(_path.c_str()) < 0)
+    {
+        if (errno == ENOENT) {
+            setErrorCode(404);
+        } else if (errno == EACCES || errno == EPERM) {
+            setErrorCode(403);
+        } else if (errno == EINVAL) {
+            setErrorCode(400);
+        } else if (errno == EBUSY) {
+            setErrorCode(503);
+        } else {
+            setErrorCode(500);
+        }
+        return false;
+    }
+    return true;
 }
