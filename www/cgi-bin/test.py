8ba55bb4633f5a61ddd089e2317dc6c431490035
Maximilien Kurek <kurekmaximilien@gmail.com>
2026-08-24 14:56:23 +0200

    fix: more cgi tests

--- www/cgi-bin/test.py
+++ www/cgi-bin/test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import os
 import sys
+import os.path
 
 print("Content-Type: text/html")
 print()
@@ -13,10 +14,16 @@ print("=== ENV VARS ===")
 for k, v in os.environ.items():
     print(f"{k}={v}")
 
+
 length = int(os.environ.get("CONTENT_LENGTH", 0) or 0)
 data = sys.stdin.read(length) if length else ""
 print("\n=== STDIN ===")
 print(data)
+print("\n=== RELATIVE PATH ===")
+if (os.path.isfile("test.sh")):
+    print("test exists")
+else:
+    print("test.sh doesnt exist")
 print("</pre>")
 
 print("</body></html>")
